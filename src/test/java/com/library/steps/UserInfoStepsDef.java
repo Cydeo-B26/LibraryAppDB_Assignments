192f1e9e2780059e60f93431eefaf497545385e2
Mehmet <mhmtsytrk@gmail.com>
2022-08-02 18:46:53 -0400

    Officehours 01

--- src/test/java/com/library/steps/UserInfoStepsDef.java
+++ src/test/java/com/library/steps/UserInfoStepsDef.java
@@ -58,9 +58,11 @@ public class UserInfoStepsDef {
     public void execute_query_to_get_all_columns() {
         DB_Util.runQuery("select * from users");
         actualList = DB_Util.getAllColumnNamesAsList();
+        System.out.println("actualList = " + actualList);
     }
     @Then("verify the below columns are listed in result")
     public void verify_the_below_columns_are_listed_in_result(List<String> expectedList) {
             Assert.assertEquals(expectedList,actualList);
+        System.out.println("expectedList = " + expectedList);
     }
 }
--- src/test/resources/features/us01.feature
+++ src/test/resources/features/us01.feature
@@ -8,7 +8,7 @@ Feature: As a data consumer, I want the user information are stored in mySql DB 
     When Execute query to get all IDs from users
     Then verify all users has unique ID
 
-  @db
+  @db@ui
   Scenario: verify users table columns
     When Execute query to get all columns
     Then verify the below columns are listed in result
